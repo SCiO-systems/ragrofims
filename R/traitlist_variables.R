5f353ecd9f6fa7409c175918e39dbe05c6992088
omarbenites <omar_cs_7@hotmail.com>
2020-11-05 23:24:30 -0500

    add VariableId and remove AgroFIMS ID

--- R/traitlist_variables.R
+++ R/traitlist_variables.R
@@ -62,7 +62,7 @@ get_agrofims_traitlist <- function(expsiteId=NULL,
     }
     
     #traitlist_dt$AgroFIMSId <- 1:nrow(traitlist_dt)
-    traitlist_dt$VariableId <- 1:nrow(traitlist_dt)
+    
     
     
   } else{
